38c6f799bb92ad7ff478d1730bff451fa2887955
Sándor Szőke <SandorS@topdesk.com>
2021-08-27 18:16:58 +0200

    vuetify added, stepper being implemented

---
+++ src/components/UserInputs.vue
@@ -0,0 +1,131 @@
+<template>
+  <v-stepper
+    v-model="e6"
+    vertical
+  >
+    <v-stepper-step
+      :complete="e6 > 1"
+      step="1"
+    >
+      Select an app
+      <small>Summarize if needed</small>
+    </v-stepper-step>
+
+    <v-stepper-content step="1">
+      <v-card
+        color="grey lighten-1"
+        class="mb-12"
+        height="200px"
+      ></v-card>
+      <v-btn
+        color="primary"
+        @click="e6 = 2"
+      >
+        Continue
+      </v-btn>
+      <v-btn text>
+        Cancel
+      </v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step
+      :complete="e6 > 2"
+      step="2"
+    >
+      Configure analytics for this app
+    </v-stepper-step>
+
+    <v-stepper-content step="2">
+      <v-card
+        color="grey lighten-1"
+        class="mb-12"
+        height="200px"
+      ></v-card>
+      <v-btn
+        color="primary"
+        @click="e6 = 3"
+      >
+        Continue
+      </v-btn>
+      <v-btn text>
+        Cancel
+      </v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step
+      :complete="e6 > 3"
+      step="3"
+    >
+      Select an ad format and name ad unit
+    </v-stepper-step>
+
+    <v-stepper-content step="3">
+      <v-card
+        color="grey lighten-1"
+        class="mb-12"
+        height="200px"
+      ></v-card>
+      <v-btn
+        color="primary"
+        @click="e6 = 4"
+      >
+        Continue
+      </v-btn>
+      <v-btn text>
+        Cancel
+      </v-btn>
+    </v-stepper-content>
+
+    <v-stepper-step step="4">
+      View setup instructions
+    </v-stepper-step>
+    <v-stepper-content step="4">
+      <v-card
+        color="grey lighten-1"
+        class="mb-12"
+        height="200px"
+      ></v-card>
+      <v-btn
+        color="primary"
+        @click="e6 = 1"
+      >
+        Continue
+      </v-btn>
+      <v-btn text>
+        Cancel
+      </v-btn>
+    </v-stepper-content>
+  </v-stepper>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data () {
+      return {
+        e6: 1,
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
