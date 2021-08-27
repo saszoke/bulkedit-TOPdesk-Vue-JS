7251671a97df3c55341197702446d163f8764d45
Sándor Szőke <SandorS@topdesk.com>
2021-08-27 19:03:16 +0200

    layout almost complete

--- src/components/UserInputs.vue
+++ src/components/UserInputs.vue
@@ -1,101 +1,135 @@
 <template>
-  <v-stepper
-    v-model="e6"
-    vertical
-  >
-    <v-stepper-step
-      :complete="e6 > 1"
-      step="1"
-    >
-      Select an app
-      <small>Summarize if needed</small>
-    </v-stepper-step>
-
-    <v-stepper-content step="1">
-      <v-card
-        color="grey lighten-1"
-        class="mb-12"
-        height="200px"
-      ></v-card>
-      <v-btn
-        color="primary"
-        @click="e6 = 2"
+  <div>
+    <v-stepper
+        v-model="e6"
+        vertical
       >
-        Continue
-      </v-btn>
-      <v-btn text>
-        Cancel
-      </v-btn>
-    </v-stepper-content>
+        <v-stepper-step
+          :complete="e6 > 1"
+          step="1"
+        >
+          Enter credentials
+          <small>Summarize if needed</small>
+        </v-stepper-step>
 
-    <v-stepper-step
-      :complete="e6 > 2"
-      step="2"
-    >
-      Configure analytics for this app
-    </v-stepper-step>
+        <v-stepper-content step="1">
+          <v-card
+            color="grey lighten-1"
+            class="mb-12"
+            height="200px"
+          ></v-card>
+          <v-btn
+            color="primary"
+            @click="e6 = 2"
+          >
+            Continue
+          </v-btn>
+        </v-stepper-content>
 
-    <v-stepper-content step="2">
-      <v-card
-        color="grey lighten-1"
-        class="mb-12"
-        height="200px"
-      ></v-card>
+        <v-stepper-step
+          :complete="e6 > 2"
+          step="2"
+        >
+          Upload Incident Selection
+        </v-stepper-step>
+
+        <v-stepper-content step="2">
+          <v-card
+            color="grey lighten-1"
+            class="mb-12"
+            height="200px"
+          ></v-card>
+          <v-btn
+            color="primary"
+            @click="e6 = 3"
+          >
+            Continue
+          </v-btn>
+          <v-btn text @click="e6 = 1">
+            Back
+          </v-btn>
+        </v-stepper-content>
+
+        <v-stepper-step
+          :complete="e6 > 3"
+          step="3"
+        >
+          Confirm incident column
+        </v-stepper-step>
+
+        <v-stepper-content step="3">
+          <v-card
+            color="grey lighten-1"
+            class="mb-12"
+            height="200px"
+          ></v-card>
+          <v-btn
+            color="primary"
+            @click="e6 = 4"
+          >
+            Continue
+          </v-btn>
+          <v-btn text @click="e6 = 2">
+            Back
+          </v-btn>
+        </v-stepper-content>
+
+        <v-stepper-step :complete="e6 > 4" step="4">
+          Choose fields to be updated
+        </v-stepper-step>
+        <v-stepper-content step="4">
+          <v-card
+            color="grey lighten-1"
+            class="mb-12"
+            height="200px"
+          ></v-card>
+          <v-btn
+            color="primary"
+            @click="e6 = 5"
+          >
+            Update
+          </v-btn>
+          <v-btn text @click="e6 = 3">
+            Back
+          </v-btn>
+        </v-stepper-content>
+      </v-stepper>
+
+
+
+
+
+    <v-progress-linear
+      color="light-blue"
+      height="30"
+      :value=value
+      striped
+    ><strong>{{ Math.ceil(value) }}%</strong></v-progress-linear>
+
+
+    <div class="d-flex justify-center">
       <v-btn
-        color="primary"
-        @click="e6 = 3"
-      >
-        Continue
-      </v-btn>
-      <v-btn text>
-        Cancel
-      </v-btn>
-    </v-stepper-content>
 
-    <v-stepper-step
-      :complete="e6 > 3"
-      step="3"
-    >
-      Select an ad format and name ad unit
-    </v-stepper-step>
-
-    <v-stepper-content step="3">
-      <v-card
-        color="grey lighten-1"
-        class="mb-12"
-        height="200px"
-      ></v-card>
-      <v-btn
+        class="ma-3 text-center"
+        outlined
+        large
         color="primary"
-        @click="e6 = 4"
+        @mouseenter="()=>{
+          completeText = 'New Edit'
+          completeIcon = 'mdi-pencil-outline'
+        }"
+        @mouseleave="()=>{
+          completeText = 'Update Completed'
+          completeIcon = 'mdi-check-all'
+        }"
       >
-        Continue
+        <v-icon left>{{completeIcon}}</v-icon>
+        {{completeText}}
       </v-btn>
-      <v-btn text>
-        Cancel
-      </v-btn>
-    </v-stepper-content>
-
-    <v-stepper-step step="4">
-      View setup instructions
-    </v-stepper-step>
-    <v-stepper-content step="4">
-      <v-card
-        color="grey lighten-1"
-        class="mb-12"
-        height="200px"
-      ></v-card>
-      <v-btn
-        color="primary"
-        @click="e6 = 1"
-      >
-        Continue
-      </v-btn>
-      <v-btn text>
-        Cancel
-      </v-btn>
-    </v-stepper-content>
-  </v-stepper>
+    </div>
+    
+  </div>
+  
 </template>
 
 <script>
@@ -107,8 +141,23 @@ export default {
   data () {
       return {
         e6: 1,
+        interval: {},
+        value: 0,
+        completeText: "Update Completed",
+        completeIcon: "mdi-check-all"
       }
-    }
+    },
+  beforeDestroy () {
+    clearInterval(this.interval)
+  },
+  mounted () {
+    this.interval = setInterval(() => {
+      if (this.value === 100) {
+        return (this.value = 0)
+      }
+      this.value += 10
+    }, 1000)
+  },
 }
 </script>
 
